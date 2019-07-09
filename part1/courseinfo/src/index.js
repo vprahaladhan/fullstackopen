0217e7d93659fdf15f63a9f8f7807c30878048c2
vprahaladhan <vprahaladhan@users.noreply.github.com>
2019-07-10 07:51:16 +0800

    part 1_courseinfo_step 2
    Commit for step 2 of part1/courseinfo

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -5,7 +5,7 @@ const course = 'Half Stack application development'
 const part1 = 'Fundamentals of React'
 const exercises1 = 10
 const part2 = 'Using props to pass data'
-const exercises2 = 7
+const exercises2 = 9
 const part3 = 'State of a component'
 const exercises3 = 14
 
@@ -15,18 +15,20 @@ const Header = (props) => {
     )
 }
 
+const Part = (props) => {
+    return (
+        <p>
+            {props.name} {props.noOfExercises}
+        </p>
+    )
+}
+
 const Content = () => {
     return (
         <div>
-        <p>
-            {part1} {exercises1}
-        </p>
-        <p>
-            {part2} {exercises2}
-        </p>
-        <p>
-            {part3} {exercises3}
-        </p>
+            <Part name={part1} noOfExercises={exercises1} />
+            <Part name={part2} noOfExercises={exercises2} />
+            <Part name={part3} noOfExercises={exercises3} />
         </div>
     )
 }
