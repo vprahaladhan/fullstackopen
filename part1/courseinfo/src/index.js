288af9c96938ce447858449e7708e9bf10f12ec1
vprahaladhan <vprahaladhan@users.noreply.github.com>
2019-07-11 07:56:49 +0800

    debugging React apps
    Commit after Debugging React applications in part 1d

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { useState } from 'react'
 
 const Header = (props) => {
     return (
@@ -33,6 +34,32 @@ const Total = (props) => {
     )
 }
 
+const Display = ({ counter }) => <div>You are visitor no: {counter}</div>
+
+const OldButton = ({ btnText, counterVal }) => <button onClick={counterVal}>{btnText}</button>
+
+const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+  )
+
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        )
+    }
+  
+    return (
+        <div>
+            button press history: {props.allClicks.join(' ')}
+        </div>
+    )
+}
+  
 const App = () => {
     const course = {
         name: 'Half Stack application development',
@@ -52,6 +79,24 @@ const App = () => {
         ]
     }
 
+    const [counter, setCounter] = useState(0)
+
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+  
+    const handleLeftClick = () => {
+      setAll(allClicks.concat('L'))
+      setLeft(left + 1)
+    }
+  
+    const handleRightClick = () => {
+      setAll(allClicks.concat('R'))
+      setRight(right + 1)
+    }
+  
+    debugger
+
     return (
         <div>
             <Header course={course.name} />
@@ -60,6 +105,19 @@ const App = () => {
                         part3={course.parts[2].name} exercises3={course.parts[2].exercises} 
                         />
             <Total exercises={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
+            <Display counter={counter} />
+            <OldButton btnText="Plus" counterVal={() => setCounter(counter + 1)} />
+            <OldButton btnText="Minus" counterVal={() => setCounter(counter - 1)} />
+            <OldButton btnText="Reset" counterVal={() => setCounter(0)} />
+            <div>
+            <p>
+                {left}
+                <Button onClick={handleLeftClick} text='left' />
+                <Button onClick={handleRightClick} text='right' />
+                {right}
+            </p>
+            <History allClicks={allClicks} />
+            </div>
         </div>
     )
 }
