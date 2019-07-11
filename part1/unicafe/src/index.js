c6d29e9e2e8f5043612d066534516fd83a21a80d
vprahaladhan <vprahaladhan@users.noreply.github.com>
2019-07-11 09:17:28 +0800

    part 1_unicafe_step 6
    Commit for step 6 of part 1/unicafe

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = (props) => {
+    return (
+        <button onClick={props.onClick}>{props.feedback}</button>
+    )
+}
+
+const App = () => {
+    const good_fdbk = 'good'
+    const neutral_fdbk = 'neutral'
+    const bad_fdbk = 'bad'
+
+    // save clicks of each button to own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const incGoodFdbk = () => setGood(good + 1);
+    const incNeutralFdbk = () => setNeutral(neutral + 1);
+    const incBadFdbk = () => setBad(bad + 1);
+
+    return (
+        <div>
+            <h1>give feedback</h1>
+            <Button feedback={good_fdbk} onClick={incGoodFdbk} />
+            <Button feedback={neutral_fdbk} onClick={incNeutralFdbk} />
+            <Button feedback={bad_fdbk} onClick={incBadFdbk} />
+            <h1>statistics</h1>
+            {good_fdbk} {good}<br />
+            {neutral_fdbk} {neutral}<br />
+            {bad_fdbk} {bad}
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+export default App;
