0f0ebdbeba9a44850364165d536820584e50d70c
vprahaladhan <vprahaladhan@users.noreply.github.com>
2019-07-13 07:46:19 +0800

    part 2_coursecontents_step 10
    Commit for step 10 of part 2/coursecontents

--- part2/coursecontents/src/components/Course.js
+++ part2/coursecontents/src/components/Course.js
@@ -1,10 +1,11 @@
+// Course & other components in a separate module
 import React from 'react'
 import Header from './Header'
 import Content from './Content'
 import Total from './Total'
 
 const Course = ({ course }) => {
-        
+
     return (
         <div>
             <Header name={course.name} />
