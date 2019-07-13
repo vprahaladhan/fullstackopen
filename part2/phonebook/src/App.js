434f131858c8e06c45b1ae7ba936e2dad73700c5
vprahaladhan <vprahaladhan@users.noreply.github.com>
2019-07-13 14:25:15 +0800

    part 2.6_thephonebook_step 1
    Commit for step 11 of part 2 - the phonebook

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [ newName, setNewName ] = useState('')
+
+  const names = () => persons.map(person => <li key={person.name}>{person.name}</li>)
+
+  const addNameToPhonebook = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({
+      name: newName
+    }))
+    setNewName('')
+  } 
+
+  const setName = (event) => setNewName(event.target.value)
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNameToPhonebook}>
+        <div>
+          name: <input value={newName} onChange={setName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {names()}
+      </ul>
+    </div>
+  )
+}
+
+export default App
