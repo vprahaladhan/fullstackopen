60fe2df2a0dde3e3f818dbcfaa2cdbb4f7d5e0de
vprahaladhan <vprahaladhan@users.noreply.github.com>
2019-07-13 15:19:16 +0800

    part 2.8_thephonebook_step 3
    Commit for step 13 of part 2/thephonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,27 +2,32 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '99300224' }
   ])
   const [ newName, setNewName ] = useState('')
+  const [ phoneno, setPhoneNo ] = useState('')
 
-  const names = () => persons.map(person => <li key={person.name}>{person.name}</li>)
+  const names = () => persons.map(person => <li key={person.name}>{person.name} {person.phone}</li>)
 
   const addNameToPhonebook = (event) => {
     event.preventDefault()
     persons.every(person => !person.name.includes(newName)) ? 
-      setPersons(persons.concat({name: newName})) : alert(`'${newName}' already exists in the phone book`)
+      setPersons(persons.concat({name: newName, phone: phoneno})) : alert(`'${newName}' already exists in the phone book`)
     setNewName('')
+    setPhoneNo('')
   } 
 
   const setName = (event) => setNewName(event.target.value)
+
+  const setPhoneNumber = (event) => setPhoneNo(event.target.value)
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNameToPhonebook}>
         <div>
-          name: <input value={newName} onChange={setName}/>
+          name: <input value={newName} onChange={setName}/><br/>
+          phone: <input value={phoneno} onChange={setPhoneNumber}/>
         </div>
         <div>
           <button type="submit">add</button>
