8f63c9bca53fb44b82326049361c36cf2159d874
vprahaladhan <vprahaladhan@users.noreply.github.com>
2019-07-18 08:14:01 +0800

    Update WeatherDetails.js

--- part2/countries/src/components/WeatherDetails.js
+++ part2/countries/src/components/WeatherDetails.js
@@ -5,7 +5,7 @@ const CountryDetails = ({temperature, icon, wind, direction}) => {
   return (
     <div>
       <div><b>temperature: </b><>{temperature} Celsius</></div>
-      <img src={icon} alt="Weather details" height="50" width="50"/>
+      <img src={icon} alt="Weather details" height="60" width="60"/>
       <div><b>wind: </b>{wind} kph direction {direction}</div>
     </div>
   )
